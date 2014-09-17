27c15ca30cd66bf0385a3f7352ab3bdb4b192f6a
Phillip Huang <phuang2@cs.washington.edu>
2014-09-17 08:44:18 +0800

    removed unnecessary parameter in recursive function

--- 031.py
+++ 031.py
@@ -9,19 +9,19 @@ It is possible to make £2 in the following way:
 How many different ways can £2 be made using any number of coins?
 """
 def count_ways(target, choices):
-	return count_ways_recursive(target, 0, choices, 0, {})
+	return count_ways_recursive(target, choices, 0, {})
 
-def count_ways_recursive(target, current, choices, choice_index, cache):
-	if current > target or choice_index == len(choices):
+def count_ways_recursive(remaining, choices, choice_index, cache):
+	if remaining < 0 or choice_index == len(choices):
 		return 0
-	if target == current:
+	if remaining == 0:
 		return 1
-	key1 = (current + choices[choice_index], choice_index)
+	key1 = (remaining - choices[choice_index], choice_index)
 	if key1 not in cache:
-		cache[key1] = count_ways_recursive(target, current + choices[choice_index], choices, choice_index, cache)
-	key2 = (current, choice_index + 1)
+		cache[key1] = count_ways_recursive(remaining - choices[choice_index], choices, choice_index, cache)
+	key2 = (remaining, choice_index + 1)
 	if key2 not in cache:
-		cache[key2] = count_ways_recursive(target, current, choices, choice_index + 1, cache)
+		cache[key2] = count_ways_recursive(remaining, choices, choice_index + 1, cache)
 	return cache[key1] + cache[key2]
 
 print count_ways(200, [1, 2, 5, 10, 20, 50, 100, 200])
